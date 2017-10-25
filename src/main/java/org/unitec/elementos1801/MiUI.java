3c96ad5be9ea43264b4271910ef3bb9ed7cd3441
AleBarojas98 <alehbarojas1496@hotmail.com>
2017-10-24 20:10:48 -0500

    Update MiUI.java

--- src/main/java/org/unitec/elementos1801/MiUI.java
+++ src/main/java/org/unitec/elementos1801/MiUI.java
@@ -8,10 +8,15 @@ package org.unitec.elementos1801;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.TextArea;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
@@ -22,9 +27,10 @@ import com.vaadin.ui.themes.ValoTheme;
 @Theme("valo")
 
 public class MiUI extends UI{
-
+    
+    @Autowired RepositorioMensajito repoMensa; 
     @Override
-    protected void init(VaadinRequest request) {
+    public void init(VaadinRequest request) {
         
         VerticalLayout layout=new VerticalLayout();
         
@@ -34,7 +40,38 @@ public class MiUI extends UI{
         etiqueta.addStyleName(ValoTheme.LABEL_H1);
         layout.addComponent(etiqueta);
         setContent(layout);
-                
+        
+        Label etiquetaGuardar=new Label("Aplicacion con mensajes");        
+        etiquetaGuardar.addStyleName(ValoTheme.LABEL_H2);
+        TextField textoTitulo=new TextField(); 
+        //El siguiente es un placeholder 
+        textoTitulo.setPlaceholder("Escribe el titulo");
+        TextArea textoCuerpo=new TextArea();
+        textoCuerpo.setPlaceholder("Escriba el cuerpo del mensaje");
+        Button boton=new Button("Guardar mensaje");
+        boton.addStyleName(ValoTheme.BUTTON_DANGER);
+        
+        
+        //Manejamos le evento del boton 
+        boton.addClickListener(evento->{
+            if(textoCuerpo.getValue().equals("")&&textoTitulo.getValue().equals("")){
+                Notification.show("Los campos son requeridos",Notification.Type.ERROR_MESSAGE);
+            }else{
+            
+        repoMensa.save(new Mensajito(textoTitulo.getValue(),textoCuerpo.getValue()));
+        Notification.show("Se guardo el mensaje!!", Notification.Type.ERROR_MESSAGE);
+        
+        }
+        });
+        
+        //agregamos al layout todo 
+        layout.addComponent(etiqueta); 
+        layout.addComponent(etiquetaGuardar); 
+        layout.addComponent(textoTitulo);   
+        layout.addComponent(textoCuerpo); 
+        layout.addComponent(boton);
+        setContent(layout);
+    
     }
     
     
